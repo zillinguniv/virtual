1d4982f3c5b0d9e365f6c7b56ab5924d7346bc35
zilling_universe <zillinguniverse@gmail.com>
2025-10-03 20:20:00 +0700

    Add files via upload

---
+++ cmd/vtcnode/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"strings"
+	"time"
+
+	"virtual/network/node"
+)
+
+func main() {
+	var (
+		datadir   = flag.String("datadir", ".", "data directory")
+		genesis   = flag.Int64("genesis", 0, "genesis UNIX time (UTC), default now if not set")
+		interval  = flag.Duration("interval", 2*time.Second, "mining interval, 0=disable mining")
+		p2plisten = flag.String("p2p.listen", ":30333", "p2p listen address")
+		peers     = flag.String("peers", "", "comma-separated seed peers host:port")
+		rpclisten = flag.String("rpc.listen", ":8545", "rpc http listen address")
+		extra     = flag.String("extra", "vtcnode", "extraData for header")
+	)
+	flag.Parse()
+
+	cfg := node.NodeConfig{
+		DataDir:   *datadir,
+		GenesisTs: *genesis,
+		MineEvery: *interval,
+		P2PListen: *p2plisten,
+		RPCListen: *rpclisten,
+		ExtraData: *extra,
+	}
+	if *peers != "" {
+		cfg.PeerSeeds = strings.Split(*peers, ",")
+	}
+
+	n := node.New(cfg)
+	n.InitChain()
+	log.Printf("genesisTime=%d", n.GenesisTime())
+
+	n.StartP2P()
+	n.StartMining()
+	n.StartRPC()
+
+	select {} // block forever
+}
